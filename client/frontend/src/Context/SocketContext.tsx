840941e9a1dfec746d637c863b035754a5fab266
Rahul Singh <thesincererahul@gmail.com>
2026-01-26 21:02:02 +0530

    Join new user in existing room

--- client/frontend/src/Context/SocketContext.tsx
+++ client/frontend/src/Context/SocketContext.tsx
@@ -1,17 +1,33 @@
 import SocketIoClient from "socket.io-client";
-import { createContext } from "react";
+import { createContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const WS_Server = "http://localhost:5500";
 
-const SocketContext = createContext(null);
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const SocketContext = createContext<any | null>(null);
 
-const socket = SocketIoClient(WS_Server);
+const socket = SocketIoClient(WS_Server, {
+    withCredentials: false,
+    transports: ['polling', 'websocket'],
+});
 
 interface Props {
     children: React.ReactNode;
 }
 
 export const SocketProvider: React.FC<Props> = ({ children }) => {
+    const navigate = useNavigate(); // will help to programmatically handle navigation
+    
+    useEffect(() => {
+        const enterRoom = ({ roomId } : { roomId: String }) => {
+            navigate(`/room/${roomId}`);
+        }
+
+        // we will transfer the user to the room when we collect an event of "room-created" from server
+        socket.on("room-created", enterRoom);
+    }, []);
+
     return (
         <SocketContext.Provider value={{ socket }}>
             {children}
---
+++ client/frontend/src/Pages/Room.tsx
@@ -0,0 +1,23 @@
+import { useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { SocketContext } from "../Context/SocketContext";
+
+const Room: React.FC = () => {
+    const { id } = useParams();
+
+    const { socket } = useContext(SocketContext);
+
+    useEffect(() => {
+        // emitting this event so that either creator of room or joinee in the room
+        // anyone is added the server knows that new people have been added to this room
+        socket.emit("join-room", { roomId: id });
+    }, []);
+
+    return (
+        <div>
+            room: {id}
+        </div>
+    );
+}
+
+export default Room;
